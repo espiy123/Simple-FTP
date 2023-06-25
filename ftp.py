23154dba7002882d063d8cf1f3314148ec7fe364
espiy123 <filip.pieprzyk@gmail.com>
2023-06-25 20:39:18 +0200

    added ftp connecting

--- ftp.py
+++ ftp.py
@@ -1,2 +1,20 @@
-def ftpConnect(text):
-    print(text)
+from ftplib import FTP
+class ftpConnect:
+    def __init__(self,host,user,password, *args):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.port =  args[0]
+        self.mode = args[1]
+        
+        conn = FTP(host)
+        conn.login(user=self.user,passwd=self.password)
+        
+        try:
+            conn = FTP(host)
+            conn.login(user=self.user,passwd=self.password)
+            print(conn.getwelcome())
+            print("You location on drive is: "+str(conn.pwd()))
+
+        except:
+            print("Can't connect to server")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow,QPushButton,QLineEdit,QLabel,QRadioButton
+from PyQt5.QtWidgets import QApplication, QMainWindow,QPushButton,QLineEdit,QLabel,QRadioButton,QDialog
+from PyQt5.QtGui import QIntValidator
 import sys
 from ftp import ftpConnect
 
@@ -7,33 +8,60 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        #main windows properties
         self.setWindowTitle("Simple FTP")
         self.setFixedSize(QtCore.QSize(720,480))
 
+        #connect button
         self.connectButton = QPushButton("Connect",self)
         self.connectButton.move(450,30)
-        #self.connectButton.clicked.connect(sendLoginData())
-    
+        self.connectButton.clicked.connect(self.sendLoginData)
         #host input
-        self.hostInput = QLineEdit(self).move(10,30)
-        self.hostLabel = QLabel("Host", self).move(15,5)
+        self.hostInput = QLineEdit(self)
+        self.hostInput.move(10,30)
+        self.hostLabel = QLabel("Host", self)
+        self.hostLabel.move(15,5)
         #user input
-        self.userInput = QLineEdit(self).move(120,30)
-        self.userLabel = QLabel("User", self).move(125,5)
+        self.userInput = QLineEdit(self)
+        self.userInput.move(120,30)
+        self.userLabel = QLabel("User", self)
+        self.userLabel.move(125,5)
         #password input
         self.passwordInput = QLineEdit(self)
         self.passwordInput.setEchoMode(QtWidgets.QLineEdit.Password)
         self.passwordInput.move(230,30)
-        self.passwordLabel = QLabel("Password", self).move(235,5)
+        self.passwordLabel = QLabel("Password", self)
+        self.passwordLabel.move(235,5)
         #port input
-        self.portInput = QLineEdit(self).move(10,100)
-        self.portLabel = QLabel("Port",self).move(15,75)
+        self.portInput = QLineEdit(self)
+        self.portInput.move(10,100)
+        portRange = QIntValidator()
+        portRange.setRange(0, 65535)
+        self.portInput.setValidator(portRange)
+        self.portLabel = QLabel("Port",self)
+        self.portLabel.move(15,75)
         #radio input
-        self.passiveRadio = QRadioButton(self).move(120,90)
-        self.passiveLabel = QLabel("Passive",self).move(140, 87)
-        self.activeRadio = QRadioButton(self).move(120,110)
-        self.activeLabel = QLabel("Active",self).move(140, 107)
+        self.passiveRadio = QRadioButton(self)
+        self.passiveRadio.move(120,90)
+        self.passiveRadio.setChecked(True)
+        self.passiveLabel = QLabel("Passive",self)
+        self.passiveLabel.move(140, 87)
+        self.activeRadio = QRadioButton(self)
+        self.activeRadio.move(120,110)
+        self.activeLabel = QLabel("Active",self)
+        self.activeLabel.move(140, 107)
 
+        #ftp connection
+    def sendLoginData(self):
+        host = self.hostInput.text()
+        user = self.userInput.text()
+        password = self.passwordInput.text()
+        port = self.portInput.text()
+        radioMode = self.passiveRadio.isChecked()
+        ftpConnect(host,user,password,port,radioMode)
+
+
+        
         
 app = QApplication(sys.argv)
 
